eb7d807e199e7fde0ccc4b70a04cbcedaa431e1e
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-13 20:22:37 -0300

    Update setup.py

--- Aula 13-10/Sistema Biblioteca/setup.py
+++ Aula 13-10/Sistema Biblioteca/setup.py
@@ -17,8 +17,10 @@ try:
     # cursor.execute("SELECT VERSION();")
     # resultado = cursor.fetchall()
     # print(resultado)
+    cursor.execute("DROP TABLE IF EXISTS alugueis;")
     cursor.execute("DROP TABLE IF EXISTS livros;")
     cursor.execute("DROP TABLE IF EXISTS autores;")
+    cursor.execute("DROP TABLE IF EXISTS membros;")
     con.commit()
 
     cursor.execute('''
@@ -34,7 +36,32 @@ titulo_livro varchar(255) NOT NULL,
 ano_livro integer NOT NULL,
 autor_id integer NOT NULL,
 CONSTRAINT fk_livro_autor FOREIGN KEY (autor_id) REFERENCES autores(id_autor)
-)
+);
+''')
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS membros(
+id_membro integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+nome_membro varchar(255) NOT NULL,
+email_membro varchar(255) CHECK(email_membro LIKE '%@%')
+);
+''')
+    
+    cursor.execute('''
+CREATE TABLE IF NOT EXISTS alugueis (
+id_aluguel integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+membro_id integer NOT NULL,
+livro_id integer NOT NULL,
+data_aluguel date DEFAULT current_date,
+data_devolucao date,
+CONSTRAINT fk_aluguel_membro FOREIGN KEY (membro_id) REFERENCES membros(id_membro),
+CONSTRAINT fk_aluguel_livro FOREIGN KEY (livro_id) REFERENCES livros(id_livro)      
+);
+''')
+    con.commit()
+
+    cursor.execute('''
+INSERT INTO autores
+VALUES (default, 'J.R Tolkien'), (default, 'George Martin'), (default, 'Machado de Assis');
 ''')
     con.commit()
 
