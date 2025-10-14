1ca0c2dbd7887d18ac982b9daf0cf482f723d909
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-14 19:15:50 -0300

    Update biblioteca.py

--- Aula 14-10/Sistema Biblioteca/biblioteca.py
+++ Aula 14-10/Sistema Biblioteca/biblioteca.py
@@ -2,6 +2,32 @@ import psycopg2
 import dotenv
 import os
 
+def verLivros():
+    livros = None
+
+    #Operações com Banco de Dados
+    try:
+        con = psycopg2.connect(dbname=DB_NAME, host=DB_HOST, password=DB_PASSWORD, port=DB_PORT, user=DB_USER)
+        cursor = con.cursor()
+
+        cursor.execute("SELECT * FROM livros ORDER BY id_livro ASC;")
+        livros = cursor.fetchall() 
+
+        cursor.close()
+        con.close()
+    except Exception as e:
+        print("OCORREU UM ERRO NA CONSULTA -",e)
+
+    if livros == None:
+        print("Não foi possível consultar a tabela de Livros!")
+    else:
+        print("Lista de Livros:")
+
+        print("ID | Título | Ano | Autor")
+
+        for livro in livros:
+            print(f"{livro[0]} | {livro[1]} | {livro[2]} | {livro[3]}")
+
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 DB_NAME = os.getenv("DB_NAME")
@@ -26,7 +52,7 @@ Menu:
     op = input("Digite a opção desejada:")
 
     if op == "1":
-        print("Ver Livros")
+        verLivros()
     elif op == "2":
         print("Cadastrar Livro")
     elif op == "0":
