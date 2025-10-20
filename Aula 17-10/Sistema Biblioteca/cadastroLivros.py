9ef364af39e2b7f8c9d5e2c6dc96a3211d1f65ed
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-20 19:38:04 -0300

    Update cadastroLivros.py

--- Aula 17-10/Sistema Biblioteca/cadastroLivros.py
+++ Aula 17-10/Sistema Biblioteca/cadastroLivros.py
@@ -13,6 +13,9 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_NAME")
 
+def cadastrar_livro():
+    print("Cadastro de livro acionado")
+
 meuBanco = ConexaoDB(DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD)
 
 ctk.set_appearance_mode("dark")
@@ -26,6 +29,10 @@ janela.resizable(width=False, height=False)
 formulario_livro = ctk.CTkFrame(janela)
 formulario_livro.pack(fill="x", padx = 10, pady = 10)
 
+formulario_livro.columnconfigure(0, weight=0, minsize=120)
+formulario_livro.columnconfigure(1, weight=1)
+
+
 formulario_livro_titulo_label = ctk.CTkLabel(formulario_livro, text="CADASTRAR LIVRO", font=ctk.CTkFont(size=24, weight="bold"))
 formulario_livro_titulo_label.grid(row=0, column=0, columnspan=2, pady=(0,10))
 
@@ -33,8 +40,24 @@ label_titulo_livro = ctk.CTkLabel(formulario_livro, text="TITULO:")
 label_titulo_livro.grid(row=1, column=0, sticky="w", padx=5, pady=5)
 
 campo_titulo_livro = ctk.CTkEntry(formulario_livro, width=300, placeholder_text="Digite o titulo do livro:")
-campo_titulo_livro.grid(row=1, column=1, padx=5, pady=5)
+campo_titulo_livro.grid(sticky= "ew",row=1, column=1, padx=5, pady=5)
 
 
+label_ano_livro = ctk.CTkLabel(formulario_livro, text="ANO DE LANÇAMENTO:")
+label_ano_livro.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+
+campo_ano_livro = ctk.CTkEntry(formulario_livro, width=300, placeholder_text="Digite o ano de lançamento do Livro:" )
+campo_ano_livro.grid(row=2, column=1, sticky="ew", padx=5, pady=5)
+
+label_autor_livro = ctk.CTkLabel(formulario_livro, text="AUTOR:")
+label_autor_livro.grid(row=3, column=0, sticky="w", padx=5, pady=5)
+
+lista_autor_livro = ctk.CTkComboBox(formulario_livro, width=300, state="readonly")
+lista_autor_livro.grid(row=3, column=1, sticky="ew", padx=5, pady=5)
+lista_autor_livro.configure(values=["Tolkien", "Machado de Assis", "George R. Martin"])
+lista_autor_livro.set('Tolkien')
+
+botao_cadastrar_livro = ctk.CTkButton(formulario_livro, text="Enviar", command=cadastrar_livro)
+botao_cadastrar_livro.grid(row=4, column=1, sticky="e", padx=5, pady=5)
 
 janela.mainloop()
