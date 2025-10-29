e53a8af3a2daff86a98f5d5d2f0f8adc06e89112
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-29 19:44:07 -0300

    Continuando Servidor Django

--- Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/views.py
+++ Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
+from django.http import JsonResponse, HttpResponse
 
 def hello_world(request):
-    return JsonResponse({"data": "Hello World"})
+    soma = 30+30
+
+
+    return HttpResponse(f'''
+    <h1 style="color:red">Hello World - {soma}</h1>
+    <button onclick="alert('Teste')">Clique aqui</button>
+    ''')
+
+# Crie a rota raiz '' nessa rota raiz execute uma função que exibe na tela "Bem vindo ao meu primeiro projeto Django!"
