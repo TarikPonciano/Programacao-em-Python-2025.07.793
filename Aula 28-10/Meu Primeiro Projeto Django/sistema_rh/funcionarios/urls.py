33ece03d3f2a48fca2b889e0671dbe4b4209d440
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-10-30 18:55:49 -0300

    Cadastro

--- Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/urls.py
+++ Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name="home"),
+    path('cadastro', views.cadastro_funcionario, name="cadastro"),
     path('hello/', views.hello_world, name="hello")
 ]
--- Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/views.py
+++ Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/views.py
@@ -4,7 +4,6 @@ from django.http import JsonResponse, HttpResponse
 def hello_world(request):
     soma = 30+30
 
-
     return HttpResponse(f'''
     <h1 style="color:red">Hello World - {soma}</h1>
     <button onclick="alert('Teste')">Clique aqui</button>
@@ -13,5 +12,7 @@ def hello_world(request):
 # Crie a rota raiz '' nessa rota raiz execute uma função que exibe na tela "Bem vindo ao meu primeiro projeto Django!"
 
 def home(request):
-
     return render(request, "funcionarios/home.html")
+
+def cadastro_funcionario(request):
+    return render(request, "funcionarios/cadastro.html")
