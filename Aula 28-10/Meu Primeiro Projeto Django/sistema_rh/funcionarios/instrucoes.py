96e9e32a522d16598adf74e6c3931df9c82147ff
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-11-03 20:38:26 -0300

    Create instrucoes.py

---
+++ Aula 28-10/Meu Primeiro Projeto Django/sistema_rh/funcionarios/instrucoes.py
@@ -0,0 +1,5 @@
+#Criar uma nova rota para o atualizar funcionarios em urls.py
+
+#Criar uma função em views que recebe o id do funcionário e realiza a atualização
+
+# Configurar o formulário para executar a rota cadastrada em urls.py
